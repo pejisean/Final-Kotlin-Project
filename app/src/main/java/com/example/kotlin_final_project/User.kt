e2eae1dd7117636932f51540c4bd2fc021396033
Jay <janjayjil@gmail.com>
2025-07-17 21:07:16 +0800

    feat: Add notes list to User class
    The `User` class now includes a `notes` property, which is a mutable list of `Note` objects, initialized as an empty list.

--- app/src/main/java/com/example/kotlin_final_project/User.kt
+++ app/src/main/java/com/example/kotlin_final_project/User.kt
@@ -4,5 +4,6 @@ data class User(
     val name: String,
     val email: String,
     val passwordHash: String,
-    val creationDate: String
+    val creationDate: String,
+    val notes: MutableList<Note> = mutableListOf()
 )
