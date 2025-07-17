b45a2c2629c056cb8479ea6dd67ceeb049e36661
Carlo Dulutan <202310776@fit.edu.ph>
2025-07-17 00:01:48 -0700

    feat: Implement SignupActivity navigation
    - Added navigation from `SignupActivity` to `LoginActivity`.
    - Pressing the "Go to Login" button now navigates to `LoginActivity` and finishes `SignupActivity`.
    - Pressing the "Signup" button now navigates to `LoginActivity`.
    - Reordered attributes for `btnGoToSignup` in `signup.xml`.

--- app/src/main/java/com/example/kotlin_final_project/SignupActivity.kt
+++ app/src/main/java/com/example/kotlin_final_project/SignupActivity.kt
@@ -1,6 +1,8 @@
 package com.example.kotlin_final_project
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,19 @@ class SignupActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnNavigateToLogin: Button = findViewById(R.id.btnGoToSignup)
+        btnNavigateToLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        val btnActualSignup: Button = findViewById(R.id.btn2signup)
+        btnActualSignup.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
