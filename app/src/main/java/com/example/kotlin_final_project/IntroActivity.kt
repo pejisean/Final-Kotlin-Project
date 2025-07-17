59b52586ba27fd7282b5d7b059b74bc3cb5caf00
Carlo Dulutan <202310776@fit.edu.ph>
2025-07-17 00:00:33 -0700

    feat: Add button to navigate to LoginActivity
    - Added a "Go to Login" button to `intro.xml`.
    - Implemented `OnClickListener` in `IntroActivity.kt` to navigate to `LoginActivity` when the button is clicked.

--- app/src/main/java/com/example/kotlin_final_project/IntroActivity.kt
+++ app/src/main/java/com/example/kotlin_final_project/IntroActivity.kt
@@ -1,6 +1,8 @@
 package com.example.kotlin_final_project
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class IntroActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnGoToLogin: Button = findViewById(R.id.btnGotoLogin)
+        btnGoToLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
 }
