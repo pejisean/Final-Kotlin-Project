20ce4114dc081fe2ab5b4b6286d98c33dfc5c314
Carlo Dulutan <202310776@fit.edu.ph>
2025-07-16 23:59:41 -0700

    feat: Add button to navigate to IntroActivity
    - Added a "Go to Intro" button to `activity_main.xml`.
    - Implemented an `OnClickListener` for the button in `MainActivity.kt` to start `IntroActivity` using an `Intent`.

--- app/src/main/java/com/example/kotlin_final_project/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_final_project/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.kotlin_final_project
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnGoToIntro: Button = findViewById(R.id.btnGotoIntro)
+        btnGoToIntro.setOnClickListener {
+            val intent = Intent(this, IntroActivity::class.java)
+            startActivity(intent)
         }
     }
 }
