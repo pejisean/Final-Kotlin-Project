b47384b831bfa6e38c81bf5dd3563ee34ffacc3c
Carlo Dulutan <202310776@fit.edu.ph>
2025-07-17 00:02:13 -0700

    feat: Implement navigation in LoginActivity
    - Added navigation from `LoginActivity` to `SignupActivity` on "Go to Signup" button click.
    - Added navigation from `LoginActivity` to `HomeActivity` on "Login" button click.

--- app/src/main/java/com/example/kotlin_final_project/LoginActivity.kt
+++ app/src/main/java/com/example/kotlin_final_project/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.kotlin_final_project
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,23 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.login)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnGoToSignup: Button = findViewById(R.id.btnGoToSignup)
+        btnGoToSignup.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnLogin: Button = findViewById(R.id.btnLogin)
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
         }
     }
 }
