8fe4cfcb9772e74f067f7b1c0ecf3e9b6a0e8be1
pejisean <202311735@fit.edu.ph>
2025-07-20 17:37:46 +0800

    feat: Create Users and Notes tables in the database

--- app/src/main/java/com/example/kotlin_final_project/DatabaseHelper.java
+++ app/src/main/java/com/example/kotlin_final_project/DatabaseHelper.java
@@ -11,25 +11,34 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     static final String DATABASE_NAME = "DIARY.db";
     static final int DATABASE_VERSION = 1;
 
+    //Users Table Building
+    static final String USERS_TABLE = "Users";
+    static final String USERS_ID = "id";
+    static final String USERS_NAME = "Name";
+    static final String USERS_EMAIL = "Email";
+    static final String USERS_PASSWORD = "Password";
+    static final String USERS_DATE = "Creation_Date";
 
     static final String CREATE_USERS_TABLE =
-            "CREATE TABLE IF NOT EXISTS User (" +
-            "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-            "name TEXT NOT NULL, " +
-            "email TEXT NOT NULL UNIQUE, " +
-            "passwordHash TEXT NOT NULL, " +
-            "creationDate TEXT NOT NULL" +
-            ");";
+            "CREATE TABLE IF NOT EXISTS " + USERS_TABLE + " (" +
+                    USERS_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    USERS_NAME + " TEXT NOT NULL, " +
+                    USERS_EMAIL + " TEXT NOT NULL UNIQUE, " +
+                    USERS_PASSWORD + " TEXT NOT NULL, " +
+                    USERS_DATE + " TEXT NOT NULL" +
+                    ");";
 
+
+    //Notes Table Building
     static final String CREATE_NOTES_TABLE =
             "CREATE TABLE IF NOT EXISTS Note (" +
-            "    id INTEGER PRIMARY KEY AUTOINCREMENT," +
-            "    userId INTEGER NOT NULL," +
-            "    title TEXT NOT NULL," +
-            "    content TEXT NOT NULL," +
-            "    date TEXT NOT NULL," +
-            "    FOREIGN KEY(userId) REFERENCES User(id) ON DELETE CASCADE" +
-            ");";
+                    "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "userId INTEGER NOT NULL, " +
+                    "title TEXT NOT NULL, " +
+                    "content TEXT NOT NULL, " +
+                    "date TEXT NOT NULL, " +
+                    "FOREIGN KEY(userId) REFERENCES Users(id) ON DELETE CASCADE" +
+                    ");";
 
     public DatabaseHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -38,6 +47,7 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_USERS_TABLE);
+        db.execSQL(CREATE_NOTES_TABLE);
     }
 
     @Override
